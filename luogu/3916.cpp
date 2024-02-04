ae698ae246df5e700060b3a4d9fc7e2f0d76e9a4
Takagi-v <caoyilu1122@gmail.com>
2024-02-04 23:52:25 +0800

    #

---
+++ luogu/3916.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+#define MAXL 100010
+
+int N, M, A[MAXL];
+vector<int> G[MAXL];
+
+void dfs(int x, int d)
+{
+  if (A[x])
+    return;
+  A[x] = d;
+  for (int i = 0; i < G[x].size(); i++)
+  {
+    dfs(G[x][i], d);
+  }
+}
+int main()
+{
+  int u, v;
+  scanf("%d%d", &N, &M);
+  for (int i = 0; i < M; i++)
+  {
+    scanf("%d%d", &u, &v);
+    G[v].push_back(u);
+  }
+  for (int i = N; i; i--)
+    dfs(i, i);
+  for (int i = 1; i <= N; i++)
+    printf("%d", A[i]);
+  printf("\n");
+  return 0;
+}
--- test.cpp
+++ test.cpp
@@ -2,6 +2,14 @@
 // It takes two integers as input and returns their sum.
 // The function name is "calculateSum".
 // Example usage: int result = calculateSum(5, 3);
-int calculateSum(int a, int b) {
+#include <iostream>
+int calculateSum(int a, int b)
+{
   return a + b;
+}
+int main()
+{
+  int a = 1;
+  int *b = &a;
+  std::cout << *b;
 }
